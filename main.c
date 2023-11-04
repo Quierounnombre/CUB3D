261d8dcebd4c8151ea6f161bc3b1c0581951383c
Vicente Garcia Andrade <vicgarci@c3r1s2.42madrid.com>
2023-11-04 14:00:17 +0100

    BASURA

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:36:16 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/11/04 13:59:48 by vicgarci         ###   ########.fr       */
+/*   Updated: 2023/11/04 14:00:12 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,5 @@ int	main(int argc, char **argv)
 	free(cube);
 	return (0);
 }
+
+
