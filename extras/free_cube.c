0db9cd85b515b3ffa6b04ce487606aa393b0db79
Vicente Garcia Andrade <vicgarci@c3r1s2.42madrid.com>
2023-11-04 14:46:15 +0100

    Free cube

---
+++ extras/free_cube.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_cube.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 14:40:45 by vicgarci          #+#    #+#             */
+/*   Updated: 2023/11/04 14:45:05 by vicgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../Cub3D.h"
+
+static void	free_map(t_map	*map)
+{
+	int		i;
+
+	i = 0;
+	while (map->map[i])
+	{
+		free(map->map[i]);
+		i++;
+	}
+	free(map->map);
+	free(map);
+}
+
+void	free_cube(t_cube	*cube)
+{
+	if (cube)
+	{
+		if (cube->cam)
+			free(cube->cam);
+		if (cube->map)
+			free_map(cube->map);
+		free(cube);
+	}
+}
