31dc6034f87344e5741ee44153b8f7d328adbe05
Vicente Garcia Andrade <vicgarci@c3r8s6.42madrid.com>
2024-01-24 15:34:03 +0100

    Fix seg fault + update defines

--- libs/define.h
+++ libs/define.h
@@ -6,7 +6,7 @@
 /*   By: vicgarci <vicgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 14:22:25 by vicgarci          #+#    #+#             */
-/*   Updated: 2023/12/15 18:15:28 by vicgarci         ###   ########.fr       */
+/*   Updated: 2024/01/24 15:32:58 by vicgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ the right number of files in the first place (ONLY 1, NOT 0, NOT 2).\n"
 //Player
 
 # define PLAYER_SPEED 0.2
-# define PLAYER_ROTATE_SPEED 0.1
+# define PLAYER_ROTATE_SPEED 0.15
 # define PLAYER_FOV 50
 # define PLAYER_DEFAULT_ANGLE 0
 # define PLAYER_DEFAULT_POS_X 0
