58708e5b6c18e90c009aa10f61e4d367d3e16471
Alfonso Garcia <alfgarci@c2r1s1.42madrid.com>
2024-03-07 17:29:18 +0100

    solo falta norminette

--- mlx/get_color_in_texture.c
+++ mlx/get_color_in_texture.c
@@ -6,7 +6,7 @@
 /*   By: alfgarci <alfgarci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:31:20 by vicgarci          #+#    #+#             */
-/*   Updated: 2024/02/29 16:18:08 by alfgarci         ###   ########.fr       */
+/*   Updated: 2024/03/07 17:26:32 by alfgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ t_color	get_color_in_texture(t_texture *texture, t_ray_info *ray)
 	if (ray->dir != 'S' && ray->dir != 'W')
 		pos_texture.x = ray->wall_ratios.y * texture->height;
 	else
-		pos_texture.x = (ray->wall_ratios.y * -1) * texture->height;
-	raw_color = *(unsigned int *)(texture->img->adres 
-		+ (pos_texture.x * (texture->img->bits_per_pixel / 8)
-		+ (pos_texture.y * texture->img->line)));
+		pos_texture.x = (1 - ray->wall_ratios.y) * texture->height;
+	raw_color = *(unsigned int *)(texture->img->adres
+			+ (pos_texture.x * (texture->img->bits_per_pixel / 8)
+				+ (pos_texture.y * texture->img->line)));
 	result = int_to_t_color(raw_color);
 	return (result);
 }
